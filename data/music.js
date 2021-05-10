92d5856949c1a2cc61c2ec64cbcf69f47f98bbf3
Derek Hudgins <hudgins.derek@gmail.com>
2021-05-10 13:31:00 -0700

    added basic outline

---
+++ data/music.js
@@ -0,0 +1,45 @@
+const smiths = {
+    id: 'smiths',
+    name: 'The Smiths',
+    image: 'smiths.png',
+    description: 'A new wave/emo band from the UK',
+    category: 'new wave',
+    price: 15.00,
+    cost: 5.00 
+};
+const growlers = {
+    id: 'growlers',
+    name: 'The Growlers',
+    image: 'growlers.png',
+    description: 'surf rock',
+    category: 'beach goth',
+    price: 15.00,
+    cost: 5.00 
+};
+const milesDavis = {
+    id: 'milesDavis',
+    name: 'Miles DAvis',
+    image: 'miles.png',
+    description: 'Jazz from the 1950s',
+    category: 'Jazz',
+    price: 15.00,
+    cost: 5.00 
+};
+const streetLight= {
+    id: 'streetLight',
+    name: 'Streetlight Manifesto',
+    image: 'streetlight.png',
+    description: 'high energy ska',
+    category: 'ska/punk',
+    price: 15.00,
+    cost: 5.00 
+};
+const specials = {
+    id: 'specials',
+    name: 'The Specials',
+    image: 'specials.png',
+    description: 'first wave ska',
+    category: 'ska',
+    price: 15.00,
+    cost: 5.00 
+};
